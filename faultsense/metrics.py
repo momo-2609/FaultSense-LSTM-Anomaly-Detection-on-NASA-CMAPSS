883954358a60d2a63ac1b093e16b8d911750f4be
Mohamed Becha <mohamedbecha55555@gmail.com>
2026-05-16 14:55:07 +0200

    Update metrics.py

--- faultsense/metrics.py
+++ faultsense/metrics.py
@@ -1,1 +1,33 @@
-
+"""
+faultsense.metrics
+------------------
+Re-exports all metric functions from the top-level metrics.py
+so they are accessible as faultsense.metrics.rmse(...) etc.
+"""
+from metrics import (
+    rmse,
+    nasa_score,
+    mean_absolute_error,
+    binary_labels,
+    binary_labels_persistent,
+    detection_lead_time,
+    early_detection_rate,
+    false_alarm_rate,
+    precision_recall_f1,
+    evaluate_detector,
+    compare_detectors,
+)
+ 
+__all__ = [
+    "rmse",
+    "nasa_score",
+    "mean_absolute_error",
+    "binary_labels",
+    "binary_labels_persistent",
+    "detection_lead_time",
+    "early_detection_rate",
+    "false_alarm_rate",
+    "precision_recall_f1",
+    "evaluate_detector",
+    "compare_detectors",
+]
